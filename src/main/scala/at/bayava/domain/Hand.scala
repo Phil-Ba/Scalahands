f87c36723a45a27d6d2bb00dce4c6cd79205f78c
Philipp Bayer <p.bayer@freemail.hu>
2015-10-01 18:17:29 +0200

    count method and tests added

--- src/main/scala/at/bayava/domain/Hand.scala
+++ src/main/scala/at/bayava/domain/Hand.scala
@@ -1,9 +1,32 @@
 package at.bayava.domain
 
+import at.bayava.domain.Colors.Color
+import at.bayava.domain.Values.Value
+
 /**
  * Created by pbayer.
  */
 class Hand(val cards: List[Card]) {
+
+  def countValueGroups(): Map[Value, Int] = {
+    cards groupBy {
+      _.value
+    } mapValues {
+      _.size
+    } filter {
+      _._2 > 1
+    }
+  }
+
+  def countColorGroups(): Map[Color, Int] = {
+    cards groupBy {
+      _.color
+    } mapValues {
+      _.size
+    } filter {
+      _._2 > 1
+    }
+  }
 
 }
 
@@ -11,11 +34,8 @@ object Hand {
   def apply(input: String): Hand = {
     require(input != null)
 
-    val cards: Array[String] = input.split(" ").to
-    require(cards.size == 5)
-
-    new Hand(cards.map {
-      (card: String) => Card(card)
-    }.toList)
+    val cards = input.split(" ").toList
+    require(cards.length == 5)
+    new Hand(cards collect { case s: String => Card(s) })
   }
 }
--- src/test/scala/at/bayava/domain/HandTest.scala
+++ src/test/scala/at/bayava/domain/HandTest.scala
@@ -1,5 +1,8 @@
 package at.bayava.domain
 
+import at.bayava.domain.Colors.{CLUBS, DIAMONDS, HEARTHS, SPADES}
+import at.bayava.domain.Values.{ACE, JACK, QUEEN}
+
 /**
  * Created by pbayer.
  */
@@ -28,6 +31,67 @@ class HandTest extends BaseScalahandsSpec {
         }
       }
     }
+
+    describe("countValueGroups") {
+
+      it("should return the count of the values") {
+        val params = Table(("cards", "groups"),
+          ("AD KS QH JC JD", Map(JACK -> 2)),
+          ("AD AS AH JC JD", Map(JACK -> 2, ACE -> 3)),
+          ("AD qS QH JC JD", Map(JACK -> 2, QUEEN -> 2))
+        )
+
+        forAll(params) { (cards, result) =>
+          val hand = Hand(cards)
+          assert(hand.countValueGroups() == result)
+        }
+      }
+
+      it("the result should not contain single values") {
+        val params = Table(("cards", "groups"),
+          ("AD KS QH TC JD", Map.empty),
+          ("AD 5S 2H 3C JD", Map.empty),
+          ("6D qS tH JC 9D", Map.empty)
+        )
+
+        forAll(params) { (cards, result) =>
+          val hand = Hand(cards)
+          assert(hand.countValueGroups() == result)
+        }
+      }
+    }
+
+    describe("countColorGroups") {
+
+      it("should return the count of the colors") {
+        val params = Table(("cards", "groups"),
+          ("AD KS QH JC JD", Map(DIAMONDS -> 2)),
+          ("AD AD AH JC JC", Map(DIAMONDS -> 2, CLUBS -> 2)),
+          ("AD qd QH Jh Jd", Map(DIAMONDS -> 3, HEARTHS -> 2))
+        )
+
+        forAll(params) { (cards, result) =>
+          val hand = Hand(cards)
+          assert(hand.countColorGroups() == result)
+        }
+      }
+
+      it("the result should not contain single colors") {
+        val params = Table(("cards", "notInList"),
+          ("AD KS QH TC JD", List(SPADES, CLUBS, HEARTHS)),
+          ("AD 5D 2H 3C Jc", List(HEARTHS))
+        )
+
+        forAll(params) { (cards, notInList) =>
+          val hand = Hand(cards)
+          val colors = hand.countColorGroups()
+          for (color <- notInList) {
+            assert(colors.contains(color) == false)
+          }
+        }
+      }
+    }
+
   }
 
 }
