e8246eca82e70e7f533d42cdc5b7e0ed27e91d55
Philipp Bayer <p.bayer@freemail.hu>
2015-10-04 19:03:09 +0200

    Tests

--- src/test/scala/at/bayava/domain/CombinationsTest.scala
+++ src/test/scala/at/bayava/domain/CombinationsTest.scala
@@ -1,6 +1,6 @@
 package at.bayava.domain
 
-import at.bayava.domain.Combinations.{Poker, RoyalFlush, Straight}
+import at.bayava.domain.Combinations._
 
 /**
  * Created by pbayer.
@@ -35,6 +35,33 @@ class CombinationsTest extends BaseScalahandsSpec {
       }
     }
 
+    it("should match full house correctly") {
+      val hands = Table(("hand", "isFullHouse"),
+        ("Ac as ah ad 2d", false),
+        ("Ac as 2h ad 2d", true),
+        ("qc 2s ah ad 2d", false),
+        ("2c 2s 3h 3d 2d", true),
+        ("3c as 4h ad 2d", false)
+      )
+      forAll(hands) { (hand, isFullHouse) =>
+        assert(Combinations.matchCombination(hand).isInstanceOf[FullHouse] == isFullHouse)
+      }
+    }
+
+    it("should match flush correctly") {
+      val hands = Table(("hand", "isFlush"),
+        ("Ad qd td qd jd", true),
+        ("8c qs th kd jd", false),
+        ("qc ts th kd jd", false),
+        ("Ac qs th 9d jd", false),
+        ("3c ad 4d 6d 2d", false),
+        ("3c ac 6c 5c 2c", true)
+      )
+      forAll(hands) { (hand, isFlush) =>
+        assert(Combinations.matchCombination(hand).isInstanceOf[Flush] == isFlush)
+      }
+    }
+
     it("should match straight correctly") {
       val hands = Table(("hand", "isStraight"),
         ("Ac qs th kd jd", true),
@@ -48,6 +75,66 @@ class CombinationsTest extends BaseScalahandsSpec {
         assert(Combinations.matchCombination(hand).isInstanceOf[Straight] == isStraight)
       }
     }
+
+    it("should match three of a kind correctly") {
+      val hands = Table(("hand", "isThreeOfAKind"),
+        ("Ac qs th kd jd", false),
+        ("8c qs th kd jd", false),
+        ("qc ts th kd jd", false),
+        ("Ac ts th td jd", true),
+        ("3c ac 4h 6s 2d", false),
+        ("3c ac 4h 4s 4d", true)
+      )
+      forAll(hands) { (hand, isThreeOfAKind) =>
+        assert(Combinations.matchCombination(hand).isInstanceOf[ThreeOfAKind] == isThreeOfAKind)
+      }
+    }
+
+    it("should match two pairs correctly") {
+      val hands = Table(("hand", "isTwoPairs"),
+        ("Ac as th td tc", false),
+        ("Ac as 4h td tc", true),
+        ("qc ts th kd kc", true),
+        ("Ac ts th td jd", false),
+        ("3c ac 4h 5s 2d", false),
+        ("3c ac 4h 4s 4d", false)
+      )
+      forAll(hands) { (hand, isTwoPairs) =>
+        assert(Combinations.matchCombination(hand).isInstanceOf[TwoPairs] == isTwoPairs)
+      }
+    }
+
+    it("should match a pair correctly") {
+      val hands = Table(("hand", "isPair"),
+        ("Ac as th td tc", false),
+        ("Ac as 4h td tc", false),
+        ("qc ts th 2d 5c", true),
+        ("Ac 2s th 2d jd", true),
+        ("3c ac 4h 5s 2d", false),
+        ("3c ac 4h 4s 4d", false)
+      )
+      forAll(hands) { (hand, isPair) =>
+        assert(Combinations.matchCombination(hand).isInstanceOf[Pair] == isPair)
+      }
+    }
+
+    it("should match a high card correctly") {
+      val hands = Table(("hand", "isHighCard"),
+        ("Ac as th td tc", false),
+        ("Ac as 4h td tc", false),
+        ("qc ts th 2d 5c", false),
+        ("Ac 2s th 2d jd", false),
+        ("3c ac 4h 5s 2d", false),
+        ("3c ac Th 5s 2d", true),
+        ("3c 6c Th 5s jd", true),
+        ("3c 2c Th qs kd", true),
+        ("3c ac 4h 4s 4d", false)
+      )
+      forAll(hands) { (hand, isHighCard) =>
+        assert(Combinations.matchCombination(hand).isInstanceOf[HighCard] == isHighCard)
+      }
+    }
+
 
   }
 }
